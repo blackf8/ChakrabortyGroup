4e2d60fb61c3b688cd86eef476b6b263989514ca
Prabu Gugagantha <44075174+blackf8@users.noreply.github.com>
2020-05-11 17:47:55 -0400

    Edits. 5/11/2020
    Fixed the double up of plots.

--- Chakraborty/particle.py
+++ Chakraborty/particle.py
@@ -4,16 +4,26 @@ import matplotlib.pyplot as plt
 import os
 # make a movie out of the various particle configs. ffmpeg
 def graphFrame(particleRadius, particleX, particleZ, counter, cwd):
-    plt.scatter(particleX, particleZ, s=particleRadius) ##look into the size variable, plotting circles.
-    # Might be over printing.
+    particleRadius = [x*25 for x in particleRadius]
+    color = [];
+    for i in range(len(particleRadius)):
+        if particleRadius[i] == 1 * 25:
+            color.append("blue");
+        elif particleRadius[i] == 1.4 * 25:
+            color.append("purple");
+        else:
+            color.append("black");
+    #look into the size variable, plotting circles.
+    plt.scatter(particleX, particleZ,c=color, s=particleRadius*20)
     plt.title('Initial Particle format ' + str(counter))
     plt.xlabel('X-Position')
     plt.ylabel('Y-Position')
     plt.savefig(cwd + "/particlePos/"+ str(counter)+".png")
+    plt.close()
     print(counter)
-    plt.show()
 
-def lineParser(currentLine, particleRadius, particleX, particleZ): #this goes through each particle in a singl frame, extracting information
+#this goes through each particle in a single frame, extracting information
+def lineParser(currentLine, particleRadius, particleX, particleZ):
     splitLine = currentLine.split(" ");
     particleRadius.append(float(splitLine[1]))
     particleX.append(float(splitLine[2]))
@@ -29,12 +39,15 @@ def main():
     particleRadius = []
     particleX = []
     particleZ = []
-    for i in range(17, (1006*60) + 17,1006*5): #130,797
+    for i in range(17, (1006*5*26) + 17,1006*5): #130,797
         temp = i
         counter = counter + 1
         for j in range(i + 6, temp + 1006):
             currentLine = listOfData[j]
             parsedLine = lineParser(currentLine, particleRadius, particleX, particleZ)
         graphFrame(particleRadius,particleX, particleZ, counter, cwd)
+        particleRadius = []
+        particleX = []
+        particleZ = []
     file.close()
 main()
