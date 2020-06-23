13e8fd32de9ca08bea8db7d59d30447aa2936d28
Prabu Gugagantha <44075174+blackf8@users.noreply.github.com>
2020-06-23 08:06:18 -0400

    Networkx2: Created FileReader Class

--- Chakraborty/networkx2.py
+++ Chakraborty/networkx2.py
@@ -1,11 +1,45 @@
-class nodeTest:
+import os
 
-    def __init__(self, datas):
-        self.data = datas
+#test
+#height of box times the cumulative strain
+#0.000214075 * 72.1195 == shift
+# y val + lz
+# x val = add the shift to the original value for x.
+class frame:
+    def __init__():
+        self.data = null
 
-    def level():
-        return data + 1
+class fileReader:
+    def __init__(self,fileName, directory):
+        self.name = fileName
+        self.dir = directory
+        self.file = open(self.dir + self.name, "r")
+        self.generator = self.generator_read()
+        self.frame = frame()
 
+    def readLine(self):
+        return self.file.readline()
 
-x =  nodeTest(3)
-print(x.data)
+    def readLines(self):
+        return self.file.readlines()
+
+    def generator_read(self):
+        for row in (self.file):
+            yield row
+
+    def next(self):
+        return self.generator.__next__()
+
+    def startUp(self):
+        for i in range(17):
+            self.next()
+
+    def frameDetails(self):
+
+cwd  = os.getcwd() #gets current path directory
+direction = "C:\\Users\\prabu\\OneDrive\Desktop\\School\\MikeInts\\Chakraborty"
+listDir = os.listdir(direction+ "/ParticleData")  # returns a list of files within this directory
+file = open(direction + "\\ParticleData/" + listDir[0], "r") #creates a file object from list listDir
+x = fileReader(listDir[0],direction + "\\ParticleData/")
+x.startUp()
+print(x.next())
