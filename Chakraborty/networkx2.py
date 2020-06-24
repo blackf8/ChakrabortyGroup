ee6a629d28b97b87e1374e86c52e523ce3f4eccb
Prabu Gugagantha <44075174+blackf8@users.noreply.github.com>
2020-06-24 00:27:02 -0400

    NetworkX2: Implementation of Frame Class

--- Chakraborty/networkx2.py
+++ Chakraborty/networkx2.py
@@ -5,30 +5,26 @@ import os
 #0.000214075 * 72.1195 == shift
 # y val = add Lz to the original value for y.
 # x val = add the shift to the original value for x.
-class frame:
-    def __init__(self):
-        self.data = None
 
-class fileReader:
+"""
+Details: The fileReader object initialization begins here.
+@param fileName: The name of the file we want to read.
+@param directory: The path directory for that file.
 
-    """
-    Details: The fileReader object initialization begins here.
-    @param fileName: The name of the file we want to read.
-    @param directory: The path directory for that file.
+Fields:
+name: The name of the file we will be reading.
+dir: The directory of the file we are reading.
+file: A file object that opens the file using self.name and self.dir.
+generator: A iterator object that will be used to parse through a file.
+#frame: A frame object that will store the file data and produce a resulting graph.
+"""
+class FileReader:
 
-    Fields:
-    name: The name of the file we will be reading.
-    dir: The directory of the file we are reading.
-    file: A file object that opens the file using self.name and self.dir.
-    generator: A iterator object that will be used to parse through a file.
-    frame: A frame object that will store the file data and produce a resulting graph.
-    """
     def __init__(self,fileName, directory):
         self.name = fileName
         self.dir = directory
         self.file = open(self.dir + self.name, "r")
         self.generator = self.generator_read()
-        self.frame = frame()
 
     """
     Details: The method readLine reads a single line from a given file.
@@ -63,7 +59,7 @@ class fileReader:
     Details: The method skip will iterate through the lines of a file a set number of times.
     @param num: The number of times you want to iterate.
     """
-    def startUp(self, num): # num = 17 for our files.
+    def skip(self, num): # num = 17 for our files.
         for i in range(num):
             self.next()
 
@@ -78,14 +74,50 @@ class fileReader:
         return packingFrac + stress
     #def frameCreator(self):
 
+
+
+"""
+Details: The frame object is used to produce a single graph with networkx/matplotlib packadges.
+There are two constructors for the different graphs we want to create depending on available data.
+@param parReader: A FileReader object used to read par files containing data on particle positions.
+@param intReader: A FileReader object used to read int files containing data on particle interations.
+
+Fields:
+frameTitle: Title for the graph/frame retrieved from the FileReader.frameDetails() method.
+parReader: The FileReader object that reads the par files.
+intreader: The FileReader object that reads the int files.
+"""
+class Frame:
+
+    def __init__(self, parReader, intReader): # postiion videos
+        self.frameTitle = parReader.frameDetails()
+        self.parReader  = parReader
+        self.intReader = intReader
+
+    def __init__(self, parReader): # network videos
+        self.frameTitle = parReader.frameDetails()
+        self.parReader  = parReader
+        self.intReader = None
+
+    def networkX(self):
+        return None
+
+    def legendData(self):
+        cumStrain = self.parReader.next().split(" ")[4]
+        print(cumStrain)
+        return None
+
+    def toString(self):
+        return self.frameTitle
+
+
 def main():
     cwd  = os.getcwd() #gets current path directory
     direction = "C:\\Users\\prabu\\OneDrive\Desktop\\School\\MikeInts\\Chakraborty"
     listDir = os.listdir(direction+ "/ParticleData")  # returns a list of files within this directory
     file = open(direction + "\\ParticleData/" + listDir[0], "r") #creates a file object from list listDir
-    x = fileReader(listDir[0],direction + "\\ParticleData/")
-    x.startUp(17)
-    print(x.next())
-    x.frameDetails()
-
+    x = FileReader(listDir[0],direction + "\\ParticleData/")
+    x.skip(17)
+    frame1 = Frame(x)
+    frame1.legendData()
 main()
