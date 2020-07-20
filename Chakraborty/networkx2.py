d162970306b7d3a63833139674432febd49490d3
Prabu Gugagantha <44075174+blackf8@users.noreply.github.com>
2020-07-20 10:10:14 -0400

    Networkx2: Autonomous
    By simply creating the FileReader, original Frame, and Video objects we can easily create videos on particle simulation by calling one method. The run method in the Video object now has the option to fully run a specific par/int file and produce the maximum number of frames for a specified experiment. Histogram of the number of  contact/non-contact forces is also plotted.

--- Chakraborty/networkx2.py
+++ Chakraborty/networkx2.py
@@ -4,20 +4,34 @@ import timeit
 import networkx as nx
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
+"""
+Program: networkx2.py
+Author: Prabu Gugagantha
+Date: Jun/21/2020
+
+Details: Networkx2 is a reimplementation of the networkx program using object oriented methods.
+This program aims to create videos of particle simulations that under-go DST.(Discontinuous Shear Thickening)
+By using objects and generators we can greatly improve the runtime and memory usage of the program, thus
+allowing the production of faster videos. At the end of this program I included some runtime tests to see how
+efficient these changes actually are.
+"""
 #test
 #height of box times the cumulative strain
 #0.000214075 * 72.1195 == shift
 # y val = add Lz to the original value for y.
-# x val = add the shift to the original value for x.
+# x val = add shift to the original value for x.
 """"
 Things to improve/ Things to add
 --> Show portions of graphs, e.x: partial domains[x1,x2]
---> Fix how the force time series is called in video
+-*> Fix how the force time series is called in video
 --> Add/Calculate boundary particles
-
-
-
+-*> Automate the video making process
+    -*> Allow for one video to be created automatically without manually inputting amount of Frames
+    -*> Create a loop that will create multiple videos.
+-*> Test all the videos in the database
 """
+
+
 """
 Details: The fileReader object initialization begins here.
 @param fileName: The name of the file we want to read.
@@ -67,7 +81,11 @@ class FileReader:
     @return self.generator.__next__(): Returns the next line after calling __next__().
     """
     def next(self):
-        return self.generator.__next__()
+        try:
+            return self.generator.__next__()
+        except StopIteration:
+            return None
+
 
     """
     Details: The method skip will iterate through the lines of a file a set number of times.
@@ -102,34 +120,43 @@ graph: A graph object that provides the image of our frame.
 class Frame:
 
     def __init__(self, parReader, intReader = None, frameNum = None): # network videos
-        if(intReader == None):
+        self.parReader  = parReader
+        intCheck = (intReader == None)
+        if(intCheck):
             type = "Position | "
         else:
             type = "Network | "
+            self.intReader = intReader
         if(frameNum == None):
             self.frameNum = 1
+            self.parReader.skip(17)
+            if(intCheck == False):
+                self.intReader.skip(20)
         else:
             self.frameNum = frameNum
         self.frameTitle =    type + parReader.frameDetails()
-        self.parReader  = parReader
-        self.intReader = intReader
+
         self.graph = nx.Graph()
+
+    """
+    """
+    def hasNextFrame(self):
+        return not self.parReader.generator == None
+
 
     """
     Details: The method run will automatically call frame methods to produce a frame\graph.
     The if statement allows for the incorporation of the edges for our network graph.
     """
-    def run(self):
-        #self.parReader.skip(17)
-        legendElements = self.legendData()
+    def run(self,folder,line):
+        legendElements = self.legendData(line)
         self.parser()
         redCount = None
         blueCount = None
         if("Network" in self.frameTitle):
             redCount, blueCount = self.parserInt(legendElements)
-        self.networkX(legendElements)
+        self.networkX(legendElements, folder) #add a parameter that takes you to the folder. maybe the title string?
         return redCount,blueCount
-        #(undo this later!! this is for the ime series count)
 
     """
     Details: The method parserInt will clean the data from the intFile for plotting.
@@ -155,8 +182,19 @@ class Frame:
                 elif(line[2] != "0"): #contact
                     self.graph.add_edge(float(line[0]), float(line[1]), color = "red")
                     numRed = numRed + 1
+            else:
+                """
+                Here is where we would add the boundary points with different colored edges
+                posInfo[first][0] = x1
+                posInfo[first][1] = y1
+                posInfo[second][0] = x2
+                posInfo[second][1] = y2
+                """
             line = self.intReader.next()
-            condition = '#' in line
+            if(not line == None):
+                condition = '#' in line #error when hits final line
+            else:
+                condition = True
         legendElements.append(Line2D([0],[0],color = 'r', label = str(numRed) + '|#Contact' , lw = 3))
         legendElements.append(Line2D([0],[0], color = 'b', label = str(numBlue) +'|#!Contact', lw=3))
         return numRed,numBlue
@@ -189,7 +227,7 @@ class Frame:
     Details: The method networkX will use the final set of data to produce our graph/frame.
     @param legendElements: A list of elements that build the legend which is added to the graph.
     """
-    def networkX(self, legendElements):
+    def networkX(self, legendElements, folder):
 
         fig = plt.figure()
         ax = fig.add_axes([.1,.1,.8,.8])
@@ -209,8 +247,11 @@ class Frame:
         nx.draw_networkx(self.graph, pos = positions,node_size = sizes, node_color = colors, edge_color = edges, with_labels = False)
         ax.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True) #used to reveil the axis numbers
         plt.legend(handles = legendElements,loc = 'upper right')#bbox_to_anchor=(1, 1));
-
-        plt.savefig("C:\\Users\\prabu\\OneDrive\Desktop\\School\\MikeInts\\Chakraborty\\FinalImages\\FrameVideo\\" + str(self.frameNum))
+        path = folder.split("/")
+        newPath = ""
+        for i in range (len(path)):
+            newPath = newPath + path[i] + "\\"
+        plt.savefig(newPath + str(self.frameNum))
         #plt.show()
         plt.close()
 
@@ -230,8 +271,9 @@ class Frame:
     Details: The method legendData produces all of the pos data needed for the legend of the graph.
     @legendElements: The legend elements saved to a list for use in the networkx() method.
     """
-    def legendData(self):
-        cumStrain = self.parReader.next().split(" ")[4]
+    def legendData(self, line):# this method will take in the first line of the frame from the video loop.
+        cumStrain = line.split(" ")[4]
+        #cumStrain = self.parReader.next().split(" ")[4]
         self.parReader.skip(1)
         stress = self.parReader.next().split(" ")[4]
         self.parReader.skip(3)
@@ -260,7 +302,7 @@ Details: The Video object is used to produce a single graph with networkx/matplo
 This constructor can be overloaded so that it can create both position and network graphs.
 @param frame: This frame will be the initial frame of our video.
 @param forceType: This variable allows you to get a timeseries of network videos.
-
+@param folder: The path of the folder we want to save the video, in string format.
 """
 class Video:
 #redundant code fix this later* Remember: matplotlib kills runtime and storage
@@ -270,17 +312,25 @@ class Video:
         if(self.forceType):
             self.x1 = []
             self.x2 = []
+
+        temp = self.frame.frameTitle.split(" ")[2]
+        parent = "D:/Chakraborty/frames/"
+        path = os.path.join(parent,temp)
+        if not os.path.exists(path):
+            os.makedirs(parent + temp)
+            self.folder = path
+
     """
     Details: The method nextFrame creates the next frame for our video.
     """
-    def nextFrame(self):
+    def nextFrame(self, line):
+        print(self.frame.frameNum)
         if(self.forceType):
-            x1,x2 = self.frame.run()
+            x1,x2 = self.frame.run(self.folder, line)
             self.x1.append(x1)
             self.x2.append(x2)
         else:
-            self.frame.run()
-        print(self.frame.frameNum)
+            self.frame.run(self.folder, line)
         cur = self.frame
         cur.incrementFrameNum()
         self.frame = Frame(cur.parReader,cur.intReader, cur.frameNum)
@@ -290,21 +340,50 @@ class Video:
     @param videoLength: The length of the video we want.
     """
     def run(self, videoLength):
-        for i in range(0, videoLength-1):
-            self.nextFrame()
-        if(self.forceType):
-            self.forceTypePlot()
+        line = self.frame.parReader.next()
+        for i in range(0, videoLength):
+            self.nextFrame(line)
+            line = self.frame.parReader.next()
+        if(videoLength == 0):
+            while(not line == None):
+                self.nextFrame(line)
+                line = self.frame.parReader.next()
+        self.forceTypePlot()
+        #self.frame.hasNextFrame()
+
 
     """
     Details: The method forceTypePlot plots a timeseries of the forces in a network video.
     """
     def forceTypePlot(self):
-        plt.plot(list(range(0,self.frame.frameNum-1)),self.x1, color = 'r', marker = 'o')
-        plt.plot(list(range(0,self.frame.frameNum-1)),self.x2, color = 'b', marker = 'o')
+
+        size = []
+        for i in range(len(self.x1)):
+            size.append(50)
+
+        plt.plot(list(range(0,self.frame.frameNum-1)),self.x1, color = 'r', marker = 'o', markersize = 5)
+        plt.plot(list(range(0,self.frame.frameNum-1)),self.x2, color = 'b', marker = 'o', markersize = 5)
         plt.xlabel("TimeStep(Frames)")
         plt.ylabel("Number of Forces")
         plt.title(self.frame.parReader.frameDetails())
-        plt.show()
+        #plt.show()
+        path = self.folder.split("/")
+        newPath = ""
+        for i in range (len(path)):
+            newPath = newPath + path[i] + "\\"
+        plt.savefig(newPath + "forceTypePlot")
+        plt.close()
+
+        fractions = []
+        for i in range(len(self.x1)):
+            fractions.append(self.x1[i]/self.x2[i])
+        plt.hist(fractions,bins = round(math.sqrt(len(fractions))))
+        plt.xlabel("Data Intervals")
+        plt.ylabel("Frequency(contact/non-contact)")
+        plt.title("Distribution: " + self.frame.parReader.frameDetails())
+
+        plt.savefig(newPath + "distribution")
+        plt.close()
 
 """
 Details: The main method is where we create objects for testing and programming purposes.
@@ -312,21 +391,20 @@ Details: The main method is where we create objects for testing and programming 
 def main():
     startTime = timeit.default_timer()
 
-    direction = "C:\\Users\\prabu\\OneDrive\Desktop\\School\\MikeInts\\Chakraborty\\ParticleData/"
-    listDir = os.listdir(direction)  # returns a list of files within this directory
-    file = open(direction+ listDir[0], "r") #creates a file object from list listDir
-    x = FileReader(listDir[0],direction)
-    x.skip(17)
+    directionData = "D:\\Chakraborty\\data/"
+    directionInt = "D:\\Chakraborty\\int/"
+    listDirData = sorted(os.listdir(directionData))# returns a list of files within this directory
+    listDirInt = sorted(os.listdir(directionInt))# returns a list of files within this directory
+    for i in range(len(listDirData)):
+        print("File Number: " + str(i))
+        print(listDirData[i])
+        print(listDirInt[i])
+        x = FileReader(listDirData[i], directionData)
+        y = FileReader(listDirInt[i], directionInt)
 
-    direction = "C:\\Users\\prabu\\OneDrive\Desktop\\School\\MikeInts\\Chakraborty\\ParticleInteration/"
-    listDir = os.listdir(direction)  # returns a list of files within this directory
-    file = open(direction + listDir[0], "r") #creates a file object from list listDir
-    y = FileReader(listDir[0], direction)
-    y.skip(20)
-
-    frame1 = Frame(x,y)
-    video = Video(frame1, True)
-    video.run(2)
+        originalFrame = Frame(x,y)
+        video = Video(originalFrame, True)
+        video.run(0)
 
     print("Runtime: ", timeit.default_timer() - startTime)
 main()
@@ -335,13 +413,22 @@ main()
 """
 Experimental Data gain from using this program against the older version 'networkx.py'
 
+Format:
+(Experiment name):
+v.# s#
+(old program)  [(num of frames, time in seconds)]
+#old:[(#f,s)]
+(new program)  [(num of frames, time in seconds)] "Sometimes the scale is written here"
+#new:[(#f,s)]
+
+
 v.8 s1
 #old:[(100f,79.88500909999999)]
-#new:[(100f,32.1224409)]
+#new:[(100f,32.1224409)]2x?
 
 v.8 s1
 #old:[(300f, 355.95)]
-#new:[(300f, 139.19)]2.5x
+#new:[(300f, 139.19)]2.5x?
 
 
 v.8 s10 "test2"
@@ -367,4 +454,21 @@ v.8 s100
 v.8 s100
 #old:[(860f,3291.9093841000004)]
 #new:[(860f,388.1169188)] 10x???
+"""
+"""
+#direction = "C:\\Users\\prabu\\OneDrive\Desktop\\School\\MikeInts\\Chakraborty\\ParticleData/"
+directionTest = "D:\\Chakraborty\\data/"
+listDir = sorted(os.listdir(directionTest))  # returns a list of files within this directory
+file = open(directionTest+ listDir[0], "r") #creates a file object from list listDir
+x = FileReader(listDir[0],directionTest)
+
+#direction = "C:\\Users\\prabu\\OneDrive\Desktop\\School\\MikeInts\\Chakraborty\\ParticleInteration/"
+directionTest = "D:\\Chakraborty\\int/"
+listDir = sorted(os.listdir(directionTest))  # returns a list of files within this directory
+file = open(directionTest + listDir[0], "r") #creates a file object from list listDir
+y = FileReader(listDir[0], directionTest)
+
+frame1 = Frame(x,y)
+video = Video(frame1, True)
+video.run(0)
 """
